b5c5013270de1187822d133bdd02faf950b9af4c
Eric MacKay <eric.mackay@gmail.com>
2017-10-09 18:17:15 -0400

    roughed in about page

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 var loadPage = function(url) {
-  var received = function() {
-    var slider = document.getElementById("page-slide");
-    slider.innerHTML = oReq.response;
-    slider.classList.add("shown");
-  };
+  var slider = document.getElementById("page-slide");
+
+  slider.classList.add("shown");
   slider.classList.remove("shown");
+
   var oReq = new XMLHttpRequest();
-  oReq.addEventListener("load", received);
+  oReq.addEventListener("load", url);
   oReq.open("GET", url);
   oReq.send();
+  slider.innerHTML = oReq.response;
 };
