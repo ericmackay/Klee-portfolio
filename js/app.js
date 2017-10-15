14dcae5ddc9a53ca7d6ad25b9550cd7e53d20994
Eric MacKay <eric.mackay@gmail.com>
2017-10-15 16:32:51 -0400

    updated styles and menu links

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ var loadPage = function(url) {
   var slider = document.getElementById("page-slide");
   var oReq = new XMLHttpRequest();
   oReq.addEventListener("load", function() {
+    1;
     var html = this.responseText;
     slider.innerHTML = html;
   });
